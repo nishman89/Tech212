a21fc1a087be5e5fb5e2c83c393df879bfa72879
nishman89 <53156243+nishman89@users.noreply.github.com>
2023-05-10 15:57:54 +0100

    Web Testing POM framework added

--- SL_TestAutomationFramework_WithSpecFlow/SL_TestAutomationFramework/lib/pages/SL_InventoryPage.cs
+++ SL_TestAutomationFramework_WithSpecFlow/SL_TestAutomationFramework/lib/pages/SL_InventoryPage.cs
@@ -9,11 +9,17 @@ namespace SL_TestAutomationFramework.lib.pages
 {
     public class SL_InventoryPage
     {
-        public SL_InventoryPage(IWebDriver seleniumDriver)
-        {
-            SeleniumDriver = seleniumDriver;
-        }
+        private IWebDriver _seleniumDriver;
+        private IWebElement _addSauceLabsBackpackToCartButton => _seleniumDriver.FindElement(By.Id("add-to-cart-sauce-labs-backpack"));
+        private IWebElement _basketCount => _seleniumDriver.FindElement(By.ClassName("shopping_cart_link"));
 
-        public IWebDriver SeleniumDriver { get; }
+        public SL_InventoryPage(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+
+        public void AddSauceLabsBackPackToBasket() => _addSauceLabsBackpackToCartButton.Click();
+        public int BasketCount()
+        {
+            var success = int.TryParse(_basketCount.Text, out int result);
+            return success ? result : 0;
+        }
     }
 }
--- SL_TestAutomationFramework_WithSpecFlow/SL_TestAutomationFramework/tests/SL_SigninStepDefinitions.cs
+++ SL_TestAutomationFramework_WithSpecFlow/SL_TestAutomationFramework/tests/SL_SigninStepDefinitions.cs
@@ -8,34 +8,10 @@ using TechTalk.SpecFlow.Assist;
 namespace SL_TestAutomationFramework.tests
 {
     [Binding]
-    public class SL_SigninStepDefinitions
+    [Scope(Feature = "SL_Signin")]
+    public class SL_SigninStepDefinitions : SharedSteps
     {
-        public SL_Website<ChromeDriver> SL_Website { get; } = new();
-        private Credentials? _credentials;
-
-        [Given(@"I am on the home page")]
-        public void GivenIAmOnTheHomePage()
-        {
-            SL_Website.SL_HomePage.VisitHomePage();
-        }
-
-        [Given(@"I have entered a valid user name")]
-        public void GivenIHaveEnteredAValidUserName()
-        {
-            SL_Website.SL_HomePage.EnterUserName(AppConfigReader.UserName);
-        }
-
-        [Given(@"I have entered a valid password")]
-        public void GivenIHaveEnteredAValidPassword()
-        {
-            SL_Website.SL_HomePage.EnterPassword(AppConfigReader.Password);
-        }
-
-        [When(@"I click the login button")]
-        public void WhenIClickTheLoginButton()
-        {
-            SL_Website.SL_HomePage.ClickLoginButton();
-        }
+        
 
         [Then(@"I should land on the inventory page")]
         public void ThenIShouldLandOnTheInventoryPage()
@@ -65,13 +41,6 @@ namespace SL_TestAutomationFramework.tests
         public void GivenIInputTheseCredentials()
         {
             SL_Website.SL_HomePage.EnterSigninCredentials(_credentials);
-        }
-
-
-        [AfterScenario]
-        public void DisposeWebDriver()
-        {
-            SL_Website.SeleniumDriver.Quit();
         }
     }
 }
